069ccaac1ed5c23002347d90ef03cce69c5e1ce7
AlexBim <centuriin@yandex.ru>
2021-10-10 17:46:38 +0300

    Добавьте файлы проекта.

---
+++ Mircroservices/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mircroservices.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string GroupName { get; set; }
+        public int Rating { get; set; }
+    }
+}
